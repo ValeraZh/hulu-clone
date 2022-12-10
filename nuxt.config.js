aadb3d7c36b3a1d271aa1203406b312e05822fee
Valera Zhuk <valerazhukvalera@mail.ru>
2022-12-10 23:51:56 +0600

    feat: made app-configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,8 +19,11 @@ export default {
     ]
   },
 
+  srcDir: 'src/',
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -28,19 +31,29 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
+  components: true,components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
+    '@nuxt/postcss8',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
   }
 }
